9aba8e3903bc235f514bb7b6dbb72a100aea9dd6
Abdulmalik Almasud <mlk1321.aa@gmail.com>
2024-03-24 12:25:44 +0300

    Item class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!.1321");
+﻿using System;
+
+namespace Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Item waterBottle = new("Water Bottle", 10, new DateTime(2023, 1, 1));
+
+                Console.WriteLine(waterBottle.GetName());
+                Console.WriteLine(waterBottle.GetQuantity());
+                Console.WriteLine(waterBottle.GetDate());
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
+
+
+
+
+class Item
+{
+    private string _name;
+    private int _quantity;
+    private DateTime _date;
+
+
+    public Item(string name, int quantity, DateTime? date = null)
+    {
+        _name = name;
+        if (quantity > 0)
+        {
+            _quantity = quantity;
+        }
+        else
+        {
+            throw new IndexOutOfRangeException("Quantity can only be positive");
+        }
+        if (date is null)
+        {
+            _date = DateTime.Now;
+        }
+        else
+        {
+            _date = (DateTime)date;
+        }
+
+    }
+
+    public string GetName() { return _name; }
+    public int GetQuantity() { return _quantity; }
+    public DateTime GetDate() { return _date; }
+}
+
